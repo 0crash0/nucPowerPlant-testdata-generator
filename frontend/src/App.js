6def9a1f7409e68f16137ef4de4be7f8160baa78
Saprin Alex <0crash0@bk.ru>
2024-04-28 03:35:03 +0300

    make svg nuc sheme more interactive
    and some cleanups

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {Slider} from "@mui/material";
 import Table from '@mui/material/Table';
-import { HexGrid, Layout, Hexagon, GridGenerator } from 'react-hexgrid';
 import axios from "axios";
 import {useEffect, useState} from "react";
 import  WaterPumpSvg from './components/WaterPump'
@@ -17,19 +16,10 @@ const SET_Ppumprate_url="http://"+SRV+"/set_ppump_rate";
 const SET_Spumprate_url="http://"+SRV+"/set_spump_rate";
 const SET_Start_url="http://"+SRV+"/start";
 const SET_Stop_url="http://"+SRV+"/stop";
-const SET_ACPpumprate_url = "http://"+SRV+"/set_acp pump_rate";
-const marks = [
-    {        value: 0,        label: '0°C',    },
-    {        value: 20,         label: '20°C',    },
-    {        value: 37,        label: '37°C',    },
-    {        value: 100,        label: '100°C',    },
-];
-function valuetext(value: number) {
-    return `${value}°C`;
-}
+const SET_ACPpumprate_url = "http://"+SRV+"/set_acppump_rate";
+
 
 function App() {
-    const hexagons = GridGenerator.hexagon(3);
     const [data,setData] = useState({
         "core_temp":247.5,
         "core_rods_pos":5,
@@ -45,6 +35,9 @@ function App() {
     const [ppumpDur, setpPumpDur] = useState("20s");
     const [spumpDur, setsPumpDur] = useState("20s");
     const [acppumpDur, setacpPumpDur] = useState("20s");
+    const [rodsPosition, setRodsPosition] = useState(0);
+    const [TurbineValve, setTurbineValve1] = useState(0);
+
     useEffect(() => {
          setInterval(() => {
         axios
@@ -79,6 +72,7 @@ function App() {
             .catch((error) => {
                 console.log(error);
             });
+        setRodsPosition(newValue);
     };
     const setPpumprate = (event, newValue) => {
         axios
@@ -125,118 +119,109 @@ function App() {
             });
     };
 
+    const setTurbineValve = (event) => {
+        /*axios
+            .get(SET_Stop_url)
+            .then((response) => response.data)
+            .catch((error) => {
+                console.log(error);
+            });*/
+        setTurbineValve1(event.target.value);
+    }
+
   return (
       <div className="App">
-        <NuclearPowerStation id="station"/>
-        <TurbineSvg id="turbine" width={70} duration={"5s"}/>
-          <Table class={'table-param'}>
-              <tr>
-                  <td>param</td>
-                  <td>slider</td>
-              </tr>
-              <tr>
-                  <td>core_rods_pos</td>
-                  <td><Slider value={data.core_rods_pos} aria-label="Default" valueLabelDisplay="auto"
-                              onChange={setRods}/></td>
-              </tr>
-              <tr>
-                  <td>Prim_Cool_pump_rate
-                      <WaterPumpSvg  id="pWaterPump" width={70} duration={ppumpDur}/>
-                  </td>
-                  <td><Slider value={data.Prim_Cool_pump_rate} aria-label="Default" valueLabelDisplay="auto"
-                              onChange={setPpumprate}/></td>
+        <NuclearPowerStation id="station" PpumpDuration={ppumpDur} SpumpDuration={spumpDur} ACpumpDuration={acppumpDur} RodsTranslate={data.core_rods_pos} ValveRotate={TurbineValve}  />
+        <TurbineSvg id="turbine" width={50} duration={"5s"}/>
+          <div name="paramsTable" style={{display: "flex", position: "absolute",  left:'25%', width: '50%'}} >
+            <Table class={'table-param'} style={{ backgroundColor:'white', width: '100%',fontSize: '14px'}}>
+                <tr>
+                    <td>param</td>
+                    <td style={{width: '30%'}}>slider</td>
+                    <td>param</td>
+                    <td style={{width: '30%'}}>slider</td>
+                </tr>
+                <tr>
+                    <td>core_rods_pos</td>
+                    <td><Slider value={data.core_rods_pos} aria-label="Default" valueLabelDisplay="auto"
+                                onChange={setRods}/></td>
+                    <td>Valve
+                    </td>
+                    <td><Slider defaultValue={0} aria-label="Default" valueLabelDisplay="auto" max={360}
+                                onChange={setTurbineValve}/></td>
+                </tr>
+                <tr>
+                    <td>Prim_Cool_pump_rate
+                        <WaterPumpSvg id="pWaterPump" width={40} duration={ppumpDur}/>
+                    </td>
+                    <td><Slider value={data.Prim_Cool_pump_rate} aria-label="Default" valueLabelDisplay="auto"
+                                onChange={setPpumprate}/></td>
+                    <td>Sec_ACP_pump_rate
+
+                        <WaterPumpSvg id="acpWaterPump" width={40} duration={acppumpDur}/>
+                    </td>
+                    <td><Slider defaultValue={data.ACP_Cool_pump_rate} aria-label="Default" valueLabelDisplay="auto"
+                                onChange={setACPpumprate}/></td>
+
+                </tr>
+                <tr>
+                    <td>Sec_Cool_pump_rate
+                        <WaterPumpSvg id="sWaterPump" width={40} duration={spumpDur}/>
+                    </td>
+                    <td><Slider defaultValue={data.Sec_Cool_pump_rate} aria-label="Default" valueLabelDisplay="auto"
+                                onChange={setSpumprate}/></td>
+                    <td style={{}}>
+                        <button onClick={setStart}>Start</button>
+                    </td>
+                    <td>
+                        <button>Pause</button> &nbsp;
+                        <button onClick={setStop}>Stop</button>
+                    </td>
+                </tr>
+
+            </Table>
+
+              <Table class={'table-param'} style={{backgroundColor: 'white',fontSize: '14px',width: '200px'}}>
+                  <tr>
+                      <td>core_temp</td>
+                      <td>{data.core_temp}</td>
+                  </tr>
+                  <tr>
+                      <td>core_rods_pos</td>
+                      <td>{data.core_rods_pos}</td>
+                  </tr>
+                  <tr>
+                      <td>Prim_Cool_pump_rate</td>
+                      <td>{data.Prim_Cool_pump_rate}</td>
+                  </tr>
+                  <tr>
+                      <td>Heat_EXCH_water_temp</td>
+                      <td>{data.Heat_EXCH_water_temp}</td>
+                  </tr>
+                  <tr>
+                      <td>Sec_Cool_pump_rate</td>
+                      <td>{data.Sec_Cool_pump_rate}</td>
+                  </tr>
+                  <tr>
+                      <td>Sec_Cool_water_temp</td>
+                      <td>{data.Sec_Cool_water_temp}</td>
+                  </tr>
+                  <tr>
+                      <td>Steam_Condencer_water_temp</td>
+                      <td>{data.Steam_Condencer_water_temp}</td>
+                  </tr>
+                  <tr>
+                      <td>ACP_Cool_pump_rate</td>
+                      <td>{data.ACP_Cool_pump_rate}</td>
+                  </tr>
+                  <tr>
+                      <td>Cooling_Tower_water_temp</td>
+                      <td>{data.Cooling_Tower_water_temp}</td>
+                  </tr>
+              </Table>
+          </div>
 
 
-              </tr>
-              <tr>
-                  <td>Sec_Cool_pump_rate
-                      <WaterPumpSvg  id="sWaterPump" width={70} duration={spumpDur}/>
-                  </td>
-                  <td><Slider defaultValue={data.Sec_Cool_pump_rate} aria-label="Default" valueLabelDisplay="auto"
-                              onChange={setSpumprate}/></td>
-              </tr>
-              <tr>
-                  <td>Sec_ACP_pump_rate
-
-                  <WaterPumpSvg  id="acpWaterPump" width={70} duration={acppumpDur}/>
-                  </td>
-                  <td><Slider defaultValue={data.ACP_Cool_pump_rate} aria-label="Default" valueLabelDisplay="auto"
-                              onChange={setACPpumprate}/></td>
-              </tr>
-          </Table>
-          <Table class={'table-param'}>
-              <tr>
-                  <td>
-                      <button onClick={setStart}>Start</button>
-                  </td>
-                  <td>
-                      <button>Pause</button>
-                  </td>
-                  <td>
-                      <button onClick={setStop}>Stop</button>
-                  </td>
-              </tr>
-          </Table>
-          <Table>
-              <tr>
-                  <td>
-                      <HexGrid width={500} height={500} fill={'red'} stroke={'black'} strokeWidth={'0.25'}>
-                          <Layout size={{x: 7, y: 7}}>
-                              {hexagons.map((hex, i) => <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}/>)}
-                          </Layout>
-                      </HexGrid>
-                  </td>
-                  <td>
-                      <Table class={'table-param'}>
-                          <tr>
-                              <td>core_temp</td>
-                              <td>{data.core_temp}</td>
-                          </tr>
-                          <tr>
-                              <td>core_rods_pos</td>
-                              <td>{data.core_rods_pos}</td>
-                          </tr>
-                          <tr>
-                              <td>Prim_Cool_pump_rate</td>
-                              <td>{data.Prim_Cool_pump_rate}</td>
-                          </tr>
-                          <tr>
-                              <td>Heat_EXCH_water_temp</td>
-                              <td>{data.Heat_EXCH_water_temp}</td>
-                          </tr>
-                          <tr>
-                              <td>Sec_Cool_pump_rate</td>
-                              <td>{data.Sec_Cool_pump_rate}</td>
-                          </tr>
-                          <tr>
-                              <td>Sec_Cool_water_temp</td>
-                              <td>{data.Sec_Cool_water_temp}</td>
-                          </tr>
-                          <tr>
-                              <td>Steam_Condencer_water_temp</td>
-                              <td>{data.Steam_Condencer_water_temp}</td>
-                          </tr>
-                          <tr>
-                              <td>ACP_Cool_pump_rate</td>
-                              <td>{data.ACP_Cool_pump_rate}</td>
-                          </tr>
-                          <tr>
-                              <td>Cooling_Tower_water_temp</td>
-                              <td>{data.Cooling_Tower_water_temp}</td>
-                          </tr>
-                      </Table>
-                  </td>
-              </tr>
-          </Table>
-
-          <Slider
-              aria-label="Custom marks"
-              defaultValue={20}
-              getAriaValueText={valuetext}
-              step={10}
-              valueLabelDisplay="auto"
-              marks={marks}
-          />
 
       </div>
   );
